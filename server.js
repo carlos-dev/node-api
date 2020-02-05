7e210c1f167784033158d19543f543e4cfcbba27
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-02-05 10:15:10 -0200

    criando base de dados com mongo

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 app.get('/', (req, res) => {
-  res.send('node api')
+  Product.create({
+    title: 'react native',
+    description: 'build app in react',
+    url: 'http://github.com/facebook/react-native'
+  })
+  return res.send('node api')
 });
 
 app.listen(3001);
